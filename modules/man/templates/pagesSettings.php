fdf80009732cfde15fa110a3646bce002402a5a3
roma <mavlutovr@ya.ru>
2018-12-03 17:11:17 +0300

    Справка - Добавил шаблоны

---
+++ modules/man/templates/pagesSettings.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'name'=>'Шапка, подвал, и т.п.',
+	'text'=>'
+		<p>Админка - Настройки - Страницы</p>
+
+<p><iframe allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" frameborder="0" height="315" src="https://www.youtube.com/embed/pGdmBiZ1lgc" width="560"></iframe></p>
+
+	',
+];
--- modules/man/templates/privacy.php
+++ modules/man/templates/privacy.php
@@ -5,7 +5,13 @@ $url = home_url().'/privacy/';
 return [
 	'name'=>'Политика конфиденциальности',
 	'text'=>'
-		<p>Должна открываться по адресу <a href="'.$url.'" target="_blank">'.$url.'</a></p>
-		<p>В тексте политики необходимо заменить __ТЕМА__, __ТЕМЕ__, __EMAIL__ на свои данные.</p>
+		<p>Должна открываться по адресу <a href="http://giraffes.laboranta.ru/privacy/" target="_blank">http://giraffes.laboranta.ru/privacy/</a></p>
+
+<p>В тексте политики необходимо заменить __ТЕМА__, __ТЕМЕ__, __EMAIL__ на свои данные.</p>
+
+<p>Подробнее в видео:&nbsp;</p>
+
+<p><iframe allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" frameborder="0" height="315" src="https://www.youtube.com/embed/BoEm0e3uLHs" width="560"></iframe></p>
+
 	',
 ];
