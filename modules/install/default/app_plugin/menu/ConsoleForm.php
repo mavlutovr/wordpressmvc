42a730085bfb90afeca33ba952700384a878027b
roma <mavlutovr@ya.ru>
2018-12-05 19:37:00 +0300

    Фикс: Чтобы по-умолчанию в Других страницах был редактор текста

---
+++ modules/install/default/app_plugin/menu/ConsoleForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Menu;
+
+class ConsoleForm extends \Wdpro\Form\Form
+{
+
+	/**
+	 * Инициализация полей
+	 *
+	 * Здесь поля добавляются в дочерних классах через $this->add(array(...)) когда они
+	 * не добавлены через конструктор
+	 */
+	protected function initFields()
+	{
+
+		$this->add([
+			'name' => 'post_content[lang]',
+			'top' => 'Текст страницы',
+			'type' => static::CKEDITOR,
+		]);
+	}
+
+
+}
