c0e18d7aea22ab2c5e99f651cabcb80557c692c1
roma <mavlutovr@ya.ru>
2019-08-05 21:45:17 +0300

    Определение поискового робота

--- inc/functions.php
+++ inc/functions.php
@@ -376,6 +376,60 @@ function wdpro_array_remove_by_value(&$array, $value) {
 
 
 /**
+ * Возвращает название поискового робота
+ *
+ * https://expange.ru/e/Определить_поискового_бота_(PHP)
+ *
+ * @param string $user_agent
+ * @return bool|mixed
+ */
+function wdpro_search_bot($user_agent=null)
+{
+
+	if ($user_agent === null) {
+		$user_agent = $_SERVER['HTTP_USER_AGENT'];
+	}
+
+
+	if (empty($user_agent)) {
+		return false;
+	}
+
+	$bots = [
+		// Yandex
+		'YandexBot', 'YandexAccessibilityBot', 'YandexMobileBot', 'YandexDirectDyn', 'YandexScreenshotBot',
+		'YandexImages', 'YandexVideo', 'YandexVideoParser', 'YandexMedia', 'YandexBlogs', 'YandexFavicons',
+		'YandexWebmaster', 'YandexPagechecker', 'YandexImageResizer', 'YandexAdNet', 'YandexDirect',
+		'YaDirectFetcher', 'YandexCalendar', 'YandexSitelinks', 'YandexMetrika', 'YandexNews',
+		'YandexNewslinks', 'YandexCatalog', 'YandexAntivirus', 'YandexMarket', 'YandexVertis',
+		'YandexForDomain', 'YandexSpravBot', 'YandexSearchShop', 'YandexMedianaBot', 'YandexOntoDB',
+		'YandexOntoDBAPI', 'YandexTurbo', 'YandexVerticals',
+
+		// Google
+		'Googlebot', 'Googlebot-Image', 'Mediapartners-Google', 'AdsBot-Google', 'APIs-Google',
+		'AdsBot-Google-Mobile', 'AdsBot-Google-Mobile', 'Googlebot-News', 'Googlebot-Video',
+		'AdsBot-Google-Mobile-Apps',
+
+		// Other
+		'Mail.RU_Bot', 'bingbot', 'Accoona', 'ia_archiver', 'Ask Jeeves', 'OmniExplorer_Bot', 'W3C_Validator',
+		'WebAlta', 'YahooFeedSeeker', 'Yahoo!', 'Ezooms', 'Tourlentabot', 'MJ12bot', 'AhrefsBot',
+		'SearchBot', 'SiteStatus', 'Nigma.ru', 'Baiduspider', 'Statsbot', 'SISTRIX', 'AcoonBot', 'findlinks',
+		'proximic', 'OpenindexSpider', 'statdom.ru', 'Exabot', 'Spider', 'SeznamBot', 'oBot', 'C-T bot',
+		'Updownerbot', 'Snoopy', 'heritrix', 'Yeti', 'DomainVader', 'DCPbot', 'PaperLiBot', 'StackRambler',
+		'msnbot', 'msnbot-media', 'msnbot-news',
+	];
+
+	foreach ($bots as $bot) {
+		if (stripos($user_agent, $bot) !== false) {
+			return $bot;
+		}
+	}
+
+	return false;
+}
+
+
+/**
  * Заменяет в адресе query данные или добавляет их
  *
  * @param string $url URL
