8bfef4bfbb1861d8965703b2ab6e44795770723d
roma <mavlutovr@ya.ru>
2019-10-29 16:14:31 +0300

    Убрал лишние теги из head

--- modules/counters/Controller.php
+++ modules/counters/Controller.php
@@ -4,6 +4,19 @@ namespace Wdpro\Counters;
 class Controller extends \Wdpro\BaseController {
 
 	/**
+	 * Инициализация модуля
+	 */
+	public static function init()
+	{
+		wdpro_ajax('countersTest', function () {
+			\Wdpro\AdminNotice\Controller::sendMessageHtml('Юзер агент',
+				print_r($_GET, 1).PHP_EOL.PHP_EOL
+			.print_r($_POST, 1));
+		});
+	}
+
+
+	/**
 	 * Выполнение скриптов после инициализаций всех модулей (в админке)
 	 */
 	public static function runConsole() {
