6db752f79fd5d579677492429efe568c114c3d6b
roma <mavlutovr@ya.ru>
2020-03-22 19:47:13 +0300

    Правка <link rel=canonical, чтобы не было дублей страниц.

--- site.php
+++ site.php
@@ -3,14 +3,13 @@
 // Редирект с index.php на корень сайта /
 if (wdpro_current_uri() === '/index.php') {
 	header('HTTP/1.1 301 Moved Permanently');
-	header('Location: '.home_url().'/');
+	header('Location: ' . home_url() . '/');
 	exit();
 }
 
 
-
 // Шорткоды
-require __DIR__.'/inc/shortcodes.php';
+require __DIR__ . '/inc/shortcodes.php';
 
 do_action('wdpro-ready');
 Wdpro\Modules::run('initSiteStart');
@@ -34,27 +33,28 @@ do_action('app-ready');
  * @param $leavename
  * @return string
  */
-function na_remove_slug( $post_link, $post, $leavename ) {
+function na_remove_slug($post_link, $post, $leavename)
+{
 
-	if ( !wdpro_is_post_type($post->post_type) && 'publish' != $post->post_status ) {
+	if (!wdpro_is_post_type($post->post_type) && 'publish' != $post->post_status) {
 		return $post_link;
 	}
 
-	$post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+	$post_link = str_replace('/' . $post->post_type . '/', '/', $post_link);
 
 	return $post_link;
 }
-add_filter( 'post_type_link', 'na_remove_slug', 10, 3 );
+
+add_filter('post_type_link', 'na_remove_slug', 10, 3);
 
 
 // Less
-if (is_file(WDPRO_TEMPLATE_PATH.'style.less'))
-{
+if (is_file(WDPRO_TEMPLATE_PATH . 'style.less')) {
 	wdpro_less_compile_try(
-		WDPRO_TEMPLATE_PATH.'style.less',
-		WDPRO_TEMPLATE_PATH.'style.less.css'
+		WDPRO_TEMPLATE_PATH . 'style.less',
+		WDPRO_TEMPLATE_PATH . 'style.less.css'
 	);
-	wdpro_add_css_to_site(WDPRO_TEMPLATE_PATH.'style.less.css');
+	wdpro_add_css_to_site(WDPRO_TEMPLATE_PATH . 'style.less.css');
 
 	// Удаляем из темы стандартный style.css
 	add_action('init', function () {
@@ -65,66 +65,64 @@ if (is_file(WDPRO_TEMPLATE_PATH.'style.less'))
 
 
 // Soy
-function themeSoyCompileAndAddToPage() {
+function themeSoyCompileAndAddToPage()
+{
 
-	$soyDir = WDPRO_TEMPLATE_PATH.'soy/';
-	$jsDir = WDPRO_TEMPLATE_PATH.'soy_compiled/';
+	$soyDir = WDPRO_TEMPLATE_PATH . 'soy/';
+	$jsDir = WDPRO_TEMPLATE_PATH . 'soy_compiled/';
 
 	if (is_dir($soyDir) && $files = scandir($soyDir)) {
-		foreach($files as $file) {
+		foreach ($files as $file) {
 			if (strstr($file, '.soy')) {
 				wdpro_closure_compile_try(
-					$soyDir.$file,
-					$jsDir.$file.'.js'
+					$soyDir . $file,
+					$jsDir . $file . '.js'
 				);
 			}
 		}
 	}
 
 	if (is_dir($jsDir) && $files = scandir($jsDir)) {
-		foreach($files as $file) {
+		foreach ($files as $file) {
 			if (strstr($file, '.soy.js')) {
-				wdpro_add_script_to_site($jsDir.$file);
+				wdpro_add_script_to_site($jsDir . $file);
 			}
 		}
 	}
 }
+
 themeSoyCompileAndAddToPage();
 
 
 // Javascript
-wdpro_add_script_to_site(__DIR__.'/js/ready.all.js');
-wdpro_add_script_to_site(__DIR__.'/js/ready.site.js');
+wdpro_add_script_to_site(__DIR__ . '/js/ready.all.js');
+wdpro_add_script_to_site(__DIR__ . '/js/ready.site.js');
 
 // Webpack
 // Скомпилированный
-if (is_file(WDPRO_TEMPLATE_PATH.'dist/main.js')) {
-	wdpro_add_script_to_site(WDPRO_TEMPLATE_PATH.'dist/main.js');
-}
-// Нескомпилированный
-else if (is_file(WDPRO_TEMPLATE_PATH.'index.js')) {
-	wdpro_add_script_to_site(WDPRO_TEMPLATE_PATH.'index.js');
+if (is_file(WDPRO_TEMPLATE_PATH . 'dist/main.js')) {
+	wdpro_add_script_to_site(WDPRO_TEMPLATE_PATH . 'dist/main.js');
+} // Нескомпилированный
+else if (is_file(WDPRO_TEMPLATE_PATH . 'index.js')) {
+	wdpro_add_script_to_site(WDPRO_TEMPLATE_PATH . 'index.js');
 }
 
-if (is_file(WDPRO_TEMPLATE_PATH.'script.js')) {
+if (is_file(WDPRO_TEMPLATE_PATH . 'script.js')) {
 
-	wdpro_add_script_to_site(WDPRO_TEMPLATE_PATH.'script.js');
+	wdpro_add_script_to_site(WDPRO_TEMPLATE_PATH . 'script.js');
 }
-if (is_file(WDPRO_TEMPLATE_PATH.'/js/script.js')) {
+if (is_file(WDPRO_TEMPLATE_PATH . '/js/script.js')) {
 
-	wdpro_add_script_to_site(WDPRO_TEMPLATE_PATH.'/js/script.js');
+	wdpro_add_script_to_site(WDPRO_TEMPLATE_PATH . '/js/script.js');
 }
 
 // app/site.js
 // По-умолчанию
 //wdpro_default_file(__DIR__.'/js/default/app.site.js', __DIR__.'/../app/site.js');
 // Подключение
-if (is_file(__DIR__.'/../app/site.js')) {
-	wdpro_add_script_to_site(__DIR__.'/../app/site.js');
+if (is_file(__DIR__ . '/../app/site.js')) {
+	wdpro_add_script_to_site(__DIR__ . '/../app/site.js');
 }
-
-
-
 
 
 /**
@@ -143,7 +141,8 @@ function appInitPage()
  *
  * @return \Wdpro\Breadcrumbs\Breadcrumbs
  */
-function breadcrumbs() {
+function breadcrumbs()
+{
 	global $breadcrumbs;
 	return $breadcrumbs;
 }
@@ -160,7 +159,8 @@ add_action('wdpro_breadcrumbs_init', '_wdpro_breadcrumbs_init', 10);
  *
  * @param \Wdpro\Breadcrumbs\Breadcrumbs $breadcrumbs Объект хлебных крошек
  */
-function _wdpro_breadcrumbs_init($breadcrumbs) {
+function _wdpro_breadcrumbs_init($breadcrumbs)
+{
 
 	if (!is_front_page()) {
 		$breadcrumbs->prependFrontPage();
@@ -183,7 +183,7 @@ wdpro_get_post(function ($post) {
 		do_action('wdpro_breadcrumbs_init', $breadcrumbs);
 
 		if (method_exists($post, 'initSite'))
-		$post->initSite();
+			$post->initSite();
 	}
 });
 
@@ -193,7 +193,8 @@ wdpro_get_post(function ($post) {
  *
  * @param callback $callback Каллбэк, в который отправляется объект хлебных крошек
  */
-function wdpro_breadcrumbs_init($callback) {
+function wdpro_breadcrumbs_init($callback)
+{
 	add_action('wdpro_breadcrumbs_init', $callback, 20);
 }
 
@@ -203,7 +204,8 @@ function wdpro_breadcrumbs_init($callback) {
  *
  * @return string
  */
-function wdpro_the_breadcrumbs() {
+function wdpro_the_breadcrumbs()
+{
 	return wdpro_breadcrumbs()->getHtml();
 }
 
@@ -224,7 +226,8 @@ function wdpro_opt($optionName)
  *
  * @return \Wdpro\BasePage
  */
-function wdpro_page() {
+function wdpro_page()
+{
 
 	global $wdproPage;
 	if (!isset($wdproPage)) {
@@ -242,7 +245,8 @@ function wdpro_page() {
  *
  * @return \Wdpro\BasePage
  */
-function wdpro_current_page() {
+function wdpro_current_page()
+{
 
 	return wdpro_page();
 }
@@ -253,7 +257,8 @@ function wdpro_current_page() {
  *
  * @param callback $callback Каллбэк, принимающий страницу
  */
-function wdpro_get_current_page($callback) {
+function wdpro_get_current_page($callback)
+{
 
 	wdpro_get_post(function ($post) use (&$callback) {
 
@@ -274,14 +279,14 @@ wdpro_get_current_page(function ($page) {
 });
 
 
-
 /**
  * Обработка открытия страницы определенного типа
  *
  * @param string $type Тип страницы
  * @param callback $callback Каллбек
  */
-function wdpro_on_page_type($type, $callback) {
+function wdpro_on_page_type($type, $callback)
+{
 	wdpro_get_current_page(function ($page) use (&$type, &$callback) {
 		/** @var \Wdpro\BasePage $page */
 
@@ -296,11 +301,12 @@ function wdpro_on_page_type($type, $callback) {
  * Запускает каллбэк при появлении контента и отправляет в каллбэк объект страницы
  * \Wdpro\BasePage
  *
- * @param string   $pageType Тип страницы \Wdpro\BasePage::getType()
+ * @param string $pageType Тип страницы \Wdpro\BasePage::getType()
  * @param callback $callback Каллбэк
- * @param int      $priority Приоритет
+ * @param int $priority Приоритет
  */
-function wdpro_on_content_type($pageType, $callback, $priority=10) {
+function wdpro_on_content_type($pageType, $callback, $priority = 10)
+{
 	wdpro_on_content_of_page_type($pageType, $callback, $priority);
 }
 
@@ -311,21 +317,19 @@ function wdpro_on_content_type($pageType, $callback, $priority=10) {
  * @param bool $echo Сразу отправить в браузер
  * @return string
  */
-function wdpro_the_content($echo=true) {
+function wdpro_the_content($echo = true)
+{
 
 	if (!$echo) {
 		ob_start();
 	}
 
-	if (have_posts())
-	{
-		while(have_posts()) {
+	if (have_posts()) {
+		while (have_posts()) {
 			the_post();
 			the_content();
 		}
-	}
-
-	else if ($GLOBALS['post']) {
+	} else if ($GLOBALS['post']) {
 		echo $GLOBALS['post']->post_content;
 	}
 
@@ -344,12 +348,12 @@ add_action('wp_enqueue_scripts', function () {
 	$post = get_post();
 
 	wp_localize_script('wdpro', 'wdproData', array(
-		'ajaxUrl'=>wdpro_ajax_url(),
-		'homeUrl'=>home_url().'/',
-		'imagesUrl'=>WDPRO_UPLOAD_IMAGES_URL,
-		'lang'=>\Wdpro\Lang\Data::getCurrentLangUri(),
-		'currentPostId'=>!empty($post->ID) ? $post->ID : '',
-		'currentPostName'=>!empty($post->post_name) ? $post->post_name : '',
+		'ajaxUrl' => wdpro_ajax_url(),
+		'homeUrl' => home_url() . '/',
+		'imagesUrl' => WDPRO_UPLOAD_IMAGES_URL,
+		'lang' => \Wdpro\Lang\Data::getCurrentLangUri(),
+		'currentPostId' => !empty($post->ID) ? $post->ID : '',
+		'currentPostName' => !empty($post->post_name) ? $post->post_name : '',
 	));
 });
 
@@ -386,13 +390,14 @@ add_filter('post_type_link', function ($post_link) {
 });*/
 
 // Чтобы кнопка Редактировать была у всех постов
-function my_admin_bar_link() {
+function my_admin_bar_link()
+{
 	global $wp_admin_bar;
 	global $post;
-	if ( !is_super_admin() || !is_admin_bar_showing() )
+	if (!is_super_admin() || !is_admin_bar_showing())
 		return;
 
-	if ( is_single() || is_page() ) {
+	if (is_single() || is_page()) {
 		$wp_admin_bar->add_menu(array(
 			'id' => 'edit',
 			'parent' => false,
@@ -401,38 +406,42 @@ function my_admin_bar_link() {
 		));
 	}
 }
-add_action( 'wp_before_admin_bar_render', 'my_admin_bar_link' );
+
+add_action('wp_before_admin_bar_render', 'my_admin_bar_link');
 
 // Удаление лишних мета-тегов
 remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wp_shortlink_wp_head');
-function at_remove_dup_canonical_link() {
+function at_remove_dup_canonical_link()
+{
 	return false;
 }
+
 //( 'wp_head',             'rel_canonical'                          );
 remove_action('wp_head', 'rest_output_link_wp_head');
-remove_action( 'wp_head', 'wp_oembed_add_discovery_links');
-remove_action( 'wp_head', 'wp_oembed_add_host_js');
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
+remove_action('wp_head', 'wp_oembed_add_host_js');
 
 
 // Отключение версий скриптов
-add_filter( 'script_loader_src', 'remove_src_version' );
-add_filter( 'style_loader_src', 'remove_src_version' );
-function remove_src_version ( $src ) {
+add_filter('script_loader_src', 'remove_src_version');
+add_filter('style_loader_src', 'remove_src_version');
+function remove_src_version($src)
+{
 
 	global $wp_version;
 
-	$version_str = '?ver='.$wp_version;
+	$version_str = '?ver=' . $wp_version;
 
 	// Меняем версию для безопасности
 	// Версия нужна только чтобы файлы новых версий закачались заново в браузер
 	$versions = explode('.', $wp_version);
-	if (isset($versions[0])) $versions[0] --;
-	if (isset($versions[2])) $versions[2] ++;
+	if (isset($versions[0])) $versions[0]--;
+	if (isset($versions[2])) $versions[2]++;
 
-	return str_replace($version_str, '?u='.implode('-', $versions), $src);
+	return str_replace($version_str, '?u=' . implode('-', $versions), $src);
 }
 
 
@@ -454,10 +463,23 @@ add_action('wp_footer', function () {
 $reCaptcha3SiteKey = get_option('wdpro_recaptcha3_site');
 if ($reCaptcha3SiteKey) {
 	wdpro_add_script_to_site_external(
-		'https://www.google.com/recaptcha/api.js?render='.$reCaptcha3SiteKey,
+		'https://www.google.com/recaptcha/api.js?render=' . $reCaptcha3SiteKey,
 		true
 	);
 	wdpro_js_data('reCaptcha3SiteKey', $reCaptcha3SiteKey);
 
-	wdpro_add_script_to_site(__DIR__.'/modules/form/recaptcha3.site.js', null, true);
+	wdpro_add_script_to_site(__DIR__ . '/modules/form/recaptcha3.site.js', null, true);
 }
+
+
+// Правка <link rel=canonical
+// Чтобы не было дублей страниц
+add_filter('get_canonical_url', function ($canonical_url, $post) {
+	if (get_query_var('cpage', 0) && $post->ID === get_queried_object_id()) {
+		$canonical_url = get_permalink($post);
+	}
+	// Добавляем в конец слеш, когда адреса со слешем в конце
+	$canonical_url .= wdpro_url_slash_at_end();
+
+	return $canonical_url;
+}, 10, 2);
