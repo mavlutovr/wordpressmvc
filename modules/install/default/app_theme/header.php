7cd7e226b78fece9d10e0af66b153382f7b46157
roma <mavlutovr@ya.ru>
2020-05-04 23:49:48 +0300

    Главная - Верх

--- modules/install/default/app_theme/header.php
+++ modules/install/default/app_theme/header.php
@@ -3,6 +3,7 @@
 <head>
 	<?php wdpro_the_header(); ?>
 	<?php wp_head(); ?>
+	<meta name=viewport content="width=device-width, initial-scale=1.0, user-scalable=no">
 </head>
 <body <?php body_class(); ?>>
 
