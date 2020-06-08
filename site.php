1bc4f0e2f55e17ab8075411bdab596e7bf14b7cc
roma <mavlutovr@ya.ru>
2020-06-08 18:54:58 +0300

    Чтобы на конце canonical не было двойных слешей

--- site.php
+++ site.php
@@ -475,12 +475,16 @@ if ($reCaptcha3SiteKey) {
 // Правка <link rel=canonical
 // Чтобы не было дублей страниц
 add_filter('get_canonical_url', function ($canonical_url, $post) {
+
 	if (get_query_var('cpage', 0) && $post->ID === get_queried_object_id()) {
 		$canonical_url = get_permalink($post);
 	}
 	// Добавляем в конец слеш, когда адреса со слешем в конце
 	if (!preg_match('~/$~', $canonical_url))
 		$canonical_url .= wdpro_url_slash_at_end();
+
+	// Еще раз убираем двойной слеш
+	$canonical_url = preg_replace('~//$~', '/', $canonical_url);
 
 	return $canonical_url;
 }, 10, 2);
