67afbcbc336b1a2fa2c586b9eeab35eb5d29955c
roma <mavlutovr@ya.ru>
2020-07-29 18:49:41 +0300

    Fix: Link to privacy_policy page from forms

--- modules/form/Elements/Privacy.php
+++ modules/form/Elements/Privacy.php
@@ -11,13 +11,13 @@ class Privacy extends Check
 		if (\Wdpro\Lang\Data::enabled()) {
 			$right = [
 
-				'ru'=>'Я даю свое согласие на обработку персональных данных и соглашаюсь с условиями и <a href="'.wdpro_home_url_with_lang(true).'/privacy-policy/" target="_blank">политикой конфиденциальности</a>.',
+				'ru'=>'Я даю свое согласие на обработку персональных данных и соглашаюсь с условиями и <a href="'.wdpro_home_url_with_lang(true).'privacy-policy/" target="_blank">политикой конфиденциальности</a>.',
 
-				'en'=>'I give my consent to the processing of personal data and agree to the terms and <a href="'.wdpro_home_url_with_lang(true).'/privacy-policy/" target="_blank">privacy policy</a>.',
+				'en'=>'I give my consent to the processing of personal data and agree to the terms and <a href="'.wdpro_home_url_with_lang(true).'privacy-policy/" target="_blank">privacy policy</a>.',
 			];
 		}
 		else {
-			$right = 'Я даю свое согласие на обработку персональных данных и соглашаюсь с условиями и <a href="'.wdpro_home_url_with_lang(true).'/privacy-policy/" target="_blank">политикой конфиденциальности</a>.';
+			$right = 'Я даю свое согласие на обработку персональных данных и соглашаюсь с условиями и <a href="'.wdpro_home_url_with_lang(true).'privacy-policy/" target="_blank">политикой конфиденциальности</a>.';
 		}
 
 		$params = wdpro_extend(array(
