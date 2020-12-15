edf46e0b4dc8bc44566b0bfeb629ed6e701c4c3c
roma <mavlutovr@ya.ru>
2020-12-15 15:25:53 +0300

    feat: pay / paypal (not finished)

---
+++ modules/pay/templates/paypal_block.old.php
@@ -0,0 +1,9 @@
+<!-- PayPal -->
+<form id="js-paypal" 
+      data-pay-id="<?php echo $data['id']; ?>" 
+      data-pay-s="<?php echo $data['secret']; ?>">
+	<div class="g-inline-block">
+		<input type="submit" value="PayPal" class="js-submit wdpro-form-submit 
+		pay-form-submit" />
+	</div>
+</form>
