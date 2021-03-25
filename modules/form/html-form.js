c301e0bd27c33d0ddabebf3cba8ac1e271e1c0d6
roma <mavlutovr@ya.ru>
2021-03-25 20:35:39 +0300

    feat: html form / sending

--- modules/form/html-form.js
+++ modules/form/html-form.js
@@ -3,15 +3,57 @@
 
   class HtmlForm {
   
-    constructor(htmlElement) {
+    constructor(htmlElement, params = { ajax: null }) {
 
       const self = this;
       
       this.$html = $(htmlElement);
+      this.$form = this.$html.is('form') ? this.$html : this.$html.find('form');
+      this.$submitButton = this.$form.find('[type="submit"]');
+      this.$messages = this.$html.find('.js-messages');
+
+      if (params.ajax === null) {
+        params.ajax = this.$form.data('ajax');
+      }
 
       // Init Elements
-      this.$html.find('.JS_element').each(function () {
+      this.$form.find('.JS_element').each(function () {
         self.initElement($(this));
+      });
+
+      // Submit
+      this.$form.on('submit', e => {
+        if (params.ajax) {
+          let url = this.$form.attr('action');
+          this.loading();
+
+          let data = this.$form.serializeObject();
+
+          wdpro.ajax(url, data, res => {
+
+            if (res.error) {
+              this.showMessage(res.error, { type: error })
+            }
+
+            else if (res.message) {
+              this.showMessage(res.message);
+            }
+
+            if (res.metrika) {
+              wdpro.yandexMetrikaGoal(res.metrika);
+            }
+
+            if (res.hideForm) {
+              if (res.message) {
+                this.$messages.css('min-height', this.$form.height());
+              }
+              this.hide();
+            }
+          });
+
+          e.preventDefault();
+          return false;
+        }
       });
     }
 
@@ -23,6 +65,43 @@
       else {
         new HtmlFormElement($element);
       }
+    }
+
+
+    hide() {
+      this.$form.hide();
+    }
+
+
+    showMessage(message, params = { type: '' }) {
+      this.$messages.empty();
+
+      if (message) {
+        let $message = $('<div/>').html(message);
+        if (params.type === 'error') {
+          $message.addClass('error');
+        }
+        this.$messages.show().append($message);
+      }
+
+      else {
+        this.$messages.hide();
+      }
+    }
+
+
+    valid() {
+
+    }
+
+
+    loading() {
+      this.$submitButton.loading();
+    }
+
+
+    loadingStop() {
+      this.$submitButton.loadingStop();
     }
     
   }
