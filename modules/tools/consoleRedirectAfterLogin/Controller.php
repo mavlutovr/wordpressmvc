b91cdaf43130f02baa4f81dcba9ad640eaef7fab
roma <mavlutovr@ya.ru>
2021-04-28 13:00:42 +0300

    fix: remove pages from Google XML Sitemaps

--- modules/tools/consoleRedirectAfterLogin/Controller.php
+++ modules/tools/consoleRedirectAfterLogin/Controller.php
@@ -12,8 +12,10 @@ class Controller extends \Wdpro\BaseController {
 
   public static function initConsole() {
     $ref = $_SERVER['HTTP_REFERER'];
+    $whlUri = get_option('whl_page');
 
-    if (strstr($ref, '/wp-login.php') && empty($_GET['logged']) && static::$uri) {
+    if ((strstr($ref, '/wp-login.php') || ($whlUri && strstr($ref, '/'.$whlUri.'/'))) 
+    && empty($_GET['logged']) && static::$uri) {
       $url = home_url() . static::$uri;
       $url = wdpro_replace_query_params_in_url($url, ['logged'=>1]);
       wdpro_location($url);
